4abb37426850fc8fe65994a8ea12bf8620ea4620
Alim Gölcük <alimglck5421@gmail.com>
2024-01-05 16:02:50 +0300

    change again

--- src/Components/Main/Footer/index.js
+++ src/Components/Main/Footer/index.js
@@ -1,93 +1,96 @@
-import React from 'react'
-import { useState } from 'react';
-import './style.css'
+import React from "react";
+import "./style.css";
 
-function Footer({listItems,setCopyListItems,All,Complete,Active,setAll,setActive,setComplete,setListItems,setCheckAllBox}) {
-  
-	// Bu fonksiyon ile All a geçiş yapılıp tüm maddelerin görünlülenmesini sağlıyoruz
-	let ClickAll = () =>{
+function Footer({
+  listItems,
+  setCopyListItems,
+  All,
+  Complete,
+  Active,
+  setAll,
+  setActive,
+  setComplete,
+  setListItems,
+  setCheckAllBox,
+}) {
+  // Bu fonksiyon ile All a geçiş yapılıp tüm maddelerin görünlülenmesini sağlıyoruz
+  let ClickAll = () => {
+    setAll("selected");
+    setComplete("");
+    setActive("");
 
-		setAll("selected");
-		setComplete("");
-		setActive("");
-		
-		let All = listItems.filter(item => item);
-		setCopyListItems([...All]);
+    let All = listItems.filter((item) => item);
+    setCopyListItems([...All]);
+  };
+  // Bu fonksiyon ile Active e geçiş yapılıp check edilmemiş maddelerin görünlülenmesini sağlıyoruz
+  let ClickActive = () => {
+    setAll("");
+    setComplete("");
+    setActive("selected");
 
-	}
-	// Bu fonksiyon ile Active e geçiş yapılıp check edilmemiş maddelerin görünlülenmesini sağlıyoruz
-	let ClickActive = () => {
+    let Active = listItems.filter((item) => item.isChecked === false);
+    setCopyListItems([...Active]);
+  };
+  // Bu fonksiyon ile Complete e geçiş yapılıp check edilmiş maddelerin görünlülenmesini sağlıyoruz
+  let ClickComplete = () => {
+    setAll("");
+    setComplete("selected");
+    setActive("");
 
-		setAll("");
-		setComplete("");
-		setActive("selected");
-		
-		let Active = listItems.filter(item => item.isChecked === false);
-		setCopyListItems([...Active]);
+    let Complete = listItems.filter((item) => item.isChecked === true);
+    setCopyListItems([...Complete]);
+  };
 
-	}
-	// Bu fonksiyon ile Complete e geçiş yapılıp check edilmiş maddelerin görünlülenmesini sağlıyoruz
-	let ClickComplete = () => {
+  let x = listItems.filter((item) => item.isChecked === false);
+  let y = listItems.length ? true : false;
+  let z = listItems.filter((item) => item.isChecked === true);
+  let f = z.length ? true : false;
 
-		setAll("");
-		setComplete("selected");
-		setActive("");
-		
-		let Complete = listItems.filter(item => item.isChecked === true);
-		setCopyListItems([...Complete]);
-	}
-	
-	let x = listItems.filter(item => item.isChecked === false);
-	let y = listItems.length ? true:false;
-	let z = listItems.filter(item => item.isChecked === true);
-	let f = z.length ? true:false;
+  //Bu fonksiyon ile checklenmiş tüm maddeleri "Clear Completed" e tıklayarak siliyoruz
+  let deleteCheckedItems = () => {
+    let updatedListy = [...listItems];
+    let Items = updatedListy.filter((item) => item.isChecked === false);
+    setListItems([...Items]);
 
-	//Bu fonksiyon ile checklenmiş tüm maddeleri "Clear Completed" e tıklayarak siliyoruz
-	let deleteCheckedItems = () => {
-		let updatedListy = [...listItems];
-		let Items =	updatedListy.filter(item => item.isChecked === false );
-		setListItems([...Items]);
+    if (!Items.length) {
+      setCheckAllBox(false);
+    }
+  };
 
-		if(!Items.length){
-			setCheckAllBox(false);
-		}
-		
-	}
-
-
-	
-
-	
-	
-	return (
-		
+  return (
     <footer className="footer" hidden={!y}>
-		<span className="todo-count">
-			<strong> {x.length} </strong>
-			items left
-		</span>
+      <span className="todo-count">
+        <strong> {x.length} </strong>
+        items left
+      </span>
 
-		
+      <ul className="filters">
+        <li>
+          <a href="#/" onClick={ClickAll} className={All}>
+            All
+          </a>
+        </li>
+        <li>
+          <a href="#/" onClick={ClickActive} className={Active}>
+            Active
+          </a>
+        </li>
+        <li>
+          <a href="#/" onClick={ClickComplete} className={Complete}>
+            Completed
+          </a>
+        </li>
+      </ul>
 
-		<ul className="filters">
-			<li>
-				<a href="#/" onClick={ClickAll} className={All} >All</a>
-			</li>
-			<li>
-				<a href="#/" onClick={ClickActive} className={Active}>Active</a>
-			</li>
-			<li>
-				<a href="#/" onClick={ClickComplete} className={Complete} >Completed</a>
-			</li>
-		</ul>
-		
-
-		<button className="clear-completed" hidden={!f} onClick={deleteCheckedItems}>
-			Clear completed
-		</button>
-	</footer>
-	
-  )
+      <button
+        className="clear-completed"
+        hidden={!f}
+        onClick={deleteCheckedItems}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
 }
 
 export default Footer;
